17e4cceff478f3b2162c16f8d1f0f9212ce9d8b8
Andrew Cao <78403245+idkuri@users.noreply.github.com>
2024-03-13 07:23:11 -0400

    created login/register forms

--- client/src/app/components/navbar.tsx
+++ client/src/app/components/navbar.tsx
@@ -1,9 +1,10 @@
 
 import Link from "next/link"
+import "./styles/navbar.css"
 export default function Navbar() {
 
     return(
-        <div className="navbar bg-base-100">
+        <div className="navbar">
             <div className="flex-1">
                 <Link className="btn btn-ghost text-xl" href = "/">Jinder</Link>
             </div>
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Jinder",
   description: "Project Description",
+  icons: {
+    icon: "/favicon.ico"
+  },
 };
 
 export default function RootLayout({
